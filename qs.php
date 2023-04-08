8a1cf25044a1a7861e46c8ec21b370426cfe1262
Angel Acuña <acuhangel@gmail.com>
2023-04-08 16:23:23 -0600

    revision 2

--- qs.php
+++ qs.php
@@ -34,30 +34,29 @@
         </section>
         <div class="division_section"></div>
 
-        <section
-          id="mision_vision "
-          class="flex column text_align_center margin_mobile_16"
-        >
-          <div class="width_100">
-            <p
-              class="intro_black_italic_italic font_size_76 text_align_center margin_10pc"
-            >
-              Vanguardia, Tecnología y el personal más calificado
+        <section id="mision_vision ">
+          <div class="flex column text_align_center margin_mobile_16">
+            <div class="width_100">
+              <p
+                class="intro_black_italic_italic font_size_76 text_align_center margin_10pc"
+              >
+                Vanguardia, Tecnología y el personal más calificado
+              </p>
+              <img
+                class="right_gray_points_up desk rotate"
+                src="assets/img/gray_points.png"
+                alt=""
+              />
+            </div>
+            <div class="division"></div>
+            <p class="intro_light_reg text_align_center margin_32">
+              Contamos con tecnología de punta y equipo vanguardista siendo un
+              proveedor confiable y competitivo ya que adquirimos el compromiso
+              de solucionar problemas en cuanto a tiempo y forma de entrega de
+              acuerdo a las necesidades de nuestros clietnes.
             </p>
-            <img
-              class="right_gray_points_up desk rotate"
-              src="assets/img/gray_points.png"
-              alt=""
-            />
+            <div></div>
           </div>
-          <div class="division"></div>
-          <p class="intro_light_reg text_align_center margin_32">
-            Contamos con tecnología de punta y equipo vanguardista siendo un
-            proveedor confiable y competitivo ya que adquirimos el compromiso de
-            solucionar problemas en cuanto a tiempo y forma de entrega de
-            acuerdo a las necesidades de nuestros clietnes.
-          </p>
-          <div></div>
         </section>
         <div class="division">
           <img
@@ -66,45 +65,47 @@
             alt=""
           />
         </div>
-        <section class="flex column text_align_center">
-          <div class="mision">
-            <p class="intro_black_italic_italic font_size_48">Misión</p>
-            <p class="font_size_16">
-              Atender los requerimientos específicos de impresión, diseño y
-              material de nuestros clientes, suministrando impresos de alta
-              calidad en el menor tiempo superando las expectativas de nuestros
-              consumidores.
-            </p>
-          </div>
-          <div class="division">
-            <div class="flex flex_space_between">
-              <div>&nbsp</div>
-              <div>
-                <img
-                  class="right_gray_points desk"
-                  src="assets/img/gray_points.png"
-                  alt=""
-                />
+        <section id="mision_vision">
+          <div class="flex column text_align_center margin_mobile_16">
+            <div class="mision">
+              <p class="intro_black_italic_italic font_size_48">Misión</p>
+              <p class="font_size_16">
+                Atender los requerimientos específicos de impresión, diseño y
+                material de nuestros clientes, suministrando impresos de alta
+                calidad en el menor tiempo superando las expectativas de
+                nuestros consumidores.
+              </p>
+            </div>
+            <div class="division">
+              <div class="flex flex_space_between">
+                <div>&nbsp</div>
+                <div>
+                  <img
+                    class="right_gray_points desk"
+                    src="assets/img/gray_points.png"
+                    alt=""
+                  />
+                </div>
               </div>
             </div>
-          </div>
-          <div class="mision">
-            <p class="intro_black_italic_italic font_size_48">Visión</p>
+            <div class="mision">
+              <p class="intro_black_italic_italic font_size_48">Visión</p>
 
-            <p class="font_size_16">
-              Ser reconocida como la mejor empresa de artes graficas a nivel
-              estatal; ser destacada a nivel nacional a través del permanente
-              compromiso con nuestros clientes, distintivo de calidad y enfoque
-              social.
-            </p>
+              <p class="font_size_16">
+                Ser reconocida como la mejor empresa de artes graficas a nivel
+                estatal; ser destacada a nivel nacional a través del permanente
+                compromiso con nuestros clientes, distintivo de calidad y
+                enfoque social.
+              </p>
+            </div>
           </div>
         </section>
 
         <div class="division_section"></div>
         <div class="division_section"></div>
 
-        <section id="valores" class="padding_left_10pc">
-          <div class="with_100 flex row">
+        <section id="valores">
+          <div class="with_100 flex row padding_left_10pc">
             <div
               class="desk gray_container flex column text_align_center flex_space_around"
             >
@@ -115,7 +116,7 @@
                   Valores
                 </p>
               </div>
-              
+
               <div class="flex row">
                 <div class="flex_item_50"></div>
                 <p class="flex_item_50">
@@ -146,7 +147,6 @@
                   <img class="" src="assets/img/checkbox.png" alt="" />
                 </p>
               </div>
-              
             </div>
             <div class="valores_list flex column flex_space_around">
               <p class="margin_16 desk">&nbsp</p>
@@ -175,7 +175,6 @@
         </section>
 
         <div class="division_section"></div>
-
         <div class="division_section">
           <img
             class="desk left_gray_points"
@@ -184,49 +183,51 @@
           />
         </div>
 
-        <section class="flex row back_gray margin_mobile_16">
-          <div class="flex row rwrap">
-            <div class="flex column rotate flex_center color_white empresa">
-              <p class="intro_black_italic_italic font_size_76 margin_0">
-                <span class="intro_book_reg font_size_48 left_menos_30"
-                  >empresa</span
-                >
-                sustentable
-              </p>
-            </div>
-            <div class="flex column flex_center text_align_center">
-              <div class="division"></div>
-              <img class="img_esr" src="assets/img/ecologia.png" alt="" />
-              <div class="division"></div>
-              <p class="intro_book_reg">
-                Nos comprometemos a operar con un enfoque de protección y
-                cuidado al medio ambiente.
-              </p>
-              <div class="division"></div>
-              <img
-                class="img_esr"
-                src="assets/img/logos pressverde-02.png"
-                alt=""
-              />
-              <div class="division"></div>
-              <p class="intro_book_reg">
-                Durante el proceso de impresión cuidamos utilizar la menor
-                cantidad de productos químicos, asimismo nos preocupamos por no
-                crear grandes cantidades de desperdicio de papel, para generar
-                un mínimo impacto medioambiental.
-              </p>
-              <div class="division"></div>
-              <img
-                class="img_esr"
-                src="assets/img/logos pressverde-02.png"
-                alt=""
-              />
-              <div class="division"></div>
-              <p class="intro_book_reg">
-                El material que resulta del desperdicio se destina para su
-                correcto reciclaje
-              </p>
-              <div class="division_section"></div>
+        <section id="empresa_sustentable">
+          <div class="flex row back_gray margin_mobile_16">
+            <div class="flex row rwrap">
+              <div class="flex column rotate flex_center color_white empresa">
+                <p class="intro_black_italic_italic font_size_76 margin_0">
+                  <span class="intro_book_reg font_size_48 left_menos_30"
+                    >empresa</span
+                  >
+                  sustentable
+                </p>
+              </div>
+              <div class="flex column flex_center text_align_center">
+                <div class="division"></div>
+                <img class="img_esr" src="assets/img/ecologia.png" alt="" />
+                <div class="division"></div>
+                <p class="intro_book_reg">
+                  Nos comprometemos a operar con un enfoque de protección y
+                  cuidado al medio ambiente.
+                </p>
+                <div class="division"></div>
+                <img
+                  class="img_esr"
+                  src="assets/img/logos pressverde-02.png"
+                  alt=""
+                />
+                <div class="division"></div>
+                <p class="intro_book_reg">
+                  Durante el proceso de impresión cuidamos utilizar la menor
+                  cantidad de productos químicos, asimismo nos preocupamos por
+                  no crear grandes cantidades de desperdicio de papel, para
+                  generar un mínimo impacto medioambiental.
+                </p>
+                <div class="division"></div>
+                <img
+                  class="img_esr"
+                  src="assets/img/logos pressverde-02.png"
+                  alt=""
+                />
+                <div class="division"></div>
+                <p class="intro_book_reg">
+                  El material que resulta del desperdicio se destina para su
+                  correcto reciclaje
+                </p>
+                <div class="division_section"></div>
+              </div>
             </div>
           </div>
         </section>
@@ -242,49 +243,48 @@
           </div>
         </div>
 
-        <section
-          id="corte_suaje/diseño"
-          class="flex column wrap wrap_desk margin_mobile_16"
-        >
-          <div class="flex row wrap">
-            <div class="flex_item_50 text_align_center">
-              <p class="intro_black_italic_italic font_size_56">
-                Comprometidos con nuestro equipo de trabajo
-              </p>
-              <p class="intro_book_alt_reg center_text_box">
-                Avanzando y juntos de la mano con nuestros trabajadores;
-                reconocemos el talento y esfuerzo de las mujeres fomentando el
-                desarrollo para un bien común.
-              </p>
-              <div class="division mobile"></div>
+        <section id="corte_suaje/diseño">
+          <div class="flex column wrap wrap_desk margin_mobile_16">
+            <div class="flex row wrap">
+              <div class="flex_item_50 text_align_center">
+                <p class="intro_black_italic_italic font_size_56">
+                  Comprometidos con nuestro equipo de trabajo
+                </p>
+                <p class="intro_book_alt_reg center_text_box">
+                  Avanzando y juntos de la mano con nuestros trabajadores;
+                  reconocemos el talento y esfuerzo de las mujeres fomentando el
+                  desarrollo para un bien común.
+                </p>
+                <div class="division mobile"></div>
+              </div>
+              <div class="flex_item_50">
+                <img
+                  class="width_100"
+                  src="assets/img/WhatsApp Image 2023-02-21 at 12.01.56 PM.png"
+                  alt=""
+                />
+              </div>
             </div>
-            <div class="flex_item_50">
-              <img
-                class="width_100"
-                src="assets/img/WhatsApp Image 2023-02-21 at 12.01.56 PM.png"
-                alt=""
-              />
-            </div>
-          </div>
 
-          <div class="flex row rwrap">
-            <div class="flex_item_50">
-              <img
-                class="width_100"
-                src="assets/img/WhatsApp Image 2023-02-21 at 12.27.10 PM.png"
-                alt=""
-              />
-            </div>
-            <div class="flex_item_50 text_align_center">
-              <p class="intro_black_italic_italic font_size_56">
-                Nos preocupamos por el bienestar de nuestros colaboradores
-              </p>
-              <p class="intro_book_alt_reg center_text_box">
-                La solidaridad con las madres solteras. y el compromiso de ser
-                un apoyo económico a través de la creación de empleos dentro de
-                Grupo Pressmedia.
-              </p>
-              <div class="division mobile"></div>
+            <div class="flex row rwrap">
+              <div class="flex_item_50">
+                <img
+                  class="width_100"
+                  src="assets/img/WhatsApp Image 2023-02-21 at 12.27.10 PM.png"
+                  alt=""
+                />
+              </div>
+              <div class="flex_item_50 text_align_center">
+                <p class="intro_black_italic_italic font_size_56">
+                  Nos preocupamos por el bienestar de nuestros colaboradores
+                </p>
+                <p class="intro_book_alt_reg center_text_box">
+                  La solidaridad con las madres solteras. y el compromiso de ser
+                  un apoyo económico a través de la creación de empleos dentro
+                  de Grupo Pressmedia.
+                </p>
+                <div class="division mobile"></div>
+              </div>
             </div>
           </div>
         </section>
